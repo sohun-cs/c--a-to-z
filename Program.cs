2df13237210997c9b927a926d83623329235135a
sohun-cs <asaduzzamansohun5@gmail.com>
2024-09-10 20:46:21 +0600

    String and String manipulation

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ Console.WriteLine("Welcome C#. It's a amazing programming language");
 Console.WriteLine($"Hello, I'm {name}");*/
 
 // Variable
-bool canICode = true;
+/*bool canICode = true;
 Console.WriteLine(canICode);
 
 Console.WriteLine("Biggest Integer : {0}", int.MaxValue);
@@ -28,3 +28,36 @@ Console.WriteLine("DEC : PI + bigNum = {0}", decPiVal + decBigNum);
 
 Console.WriteLine("Biggest Decimal : {0}", Decimal.MaxValue);
 Console.WriteLine("Smallest Decimal : {0}", Decimal.MinValue);
+*/
+
+/*Console.WriteLine("Currency : {0:c}", 23.455);
+Console.WriteLine("Pad with 0s : {0:d4}", 23);
+Console.WriteLine("3 Decimals : {0:f3}", 23.45555);
+Console.WriteLine("Commas : {0:n4}");*/
+
+
+string randomString = "This is a string";
+Console.WriteLine("String Length : {0}", randomString.Length);
+Console.WriteLine("String Contains is : {0}", randomString.Contains("is"));
+Console.WriteLine("Index of is : {0}", randomString.IndexOf("is"));
+Console.WriteLine("Remove String : {0}", randomString.Remove(10, 6));
+Console.WriteLine("Insert String : {0}", randomString.Insert(10, "short "));
+Console.WriteLine("Replace String : {0}", randomString.Replace("string", "sentence"));
+Console.WriteLine("Compare A tp B : {0}", String.Compare("A", "B", StringComparison.OrdinalIgnoreCase));
+
+Console.WriteLine("-----------------------------------------------------------------------------------");
+
+Console.WriteLine("A = a : {0}", String.Equals("A", "a"), StringComparison.OrdinalIgnoreCase);
+Console.WriteLine("Pad left : {0}", randomString.PadLeft(20, '.'));
+Console.WriteLine("Pad Right : {0}", randomString.PadRight(20, '.'));
+Console.WriteLine("Trim : {0}", randomString.Trim());
+Console.WriteLine("Uppercase : {0}", randomString.ToUpper());
+Console.WriteLine("Lowercase : {0}", randomString.ToLower());
+
+string newString = String.Format("{0} saw a {1} {2} in the {3}",
+    "Paul", "rabbit", "eating", "field");
+Console.WriteLine(newString + "\n");
+
+Console.WriteLine(@"Exactly what I typed");
+
+
